e4ddcc71a5f04f38ad03278c5ffef1ce48c81081
Wannes Salomé <mail@wannessalome.nl>
2020-02-25 23:53:04 +0100

    Add music page component

---
+++ src/pages/music.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Layout from '#components/Layout';
+import SEO from '#components/Seo';
+
+const IndexPage = () => (
+  <Layout currentPage="MUSIC">
+    <SEO title="music" />
+  </Layout>
+);
+
+export default IndexPage;
