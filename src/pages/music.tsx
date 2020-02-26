bba7615b7aabf82023d607e4463adc8d1140efd1
Wannes Salomé <mail@wannessalome.nl>
2020-02-26 01:26:00 +0100

    Fix graphql typing for music query

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -1,12 +1,43 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
 import Layout from '#components/Layout';
 import SEO from '#components/Seo';
+import { Music } from '#lib/types/__generated__/Music';
 
-const IndexPage = () => (
-  <Layout currentPage="MUSIC">
-    <SEO title="music" />
-  </Layout>
-);
+interface Props {
+  data: Music;
+}
+
+const IndexPage = ({ data }: Props) => {
+  console.log(data);
+
+  return (
+    <Layout currentPage="MUSIC">
+      <SEO title="music" />
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query Music {
+    allMusicYaml {
+      edges {
+        node {
+          id
+          links {
+            platform
+            url
+          }
+          date
+          description
+          image
+          title
+          type
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
