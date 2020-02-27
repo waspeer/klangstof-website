24eed4cf3a3cd82d246818f393852153e6c609cf
Wannes Salomé <mail@wannessalome.nl>
2020-02-27 23:58:29 +0100

    Add image test

--- src/components/MusicList/MusicList.test.tsx
+++ src/components/MusicList/MusicList.test.tsx
@@ -5,19 +5,21 @@ import factories from '#test/jest/factories';
 
 import MusicList from './MusicList';
 
+jest.mock('gatsby-image', () => () => <span data-testid="image" />);
+
 describe('MusicList', () => {
   it('should render the provided releases', () => {
-    const releases = factories.music.buildList(3);
+    const postAmount = 3;
+    const releases = factories.music.buildList(postAmount);
     const { getAllByTestId, getByText } = render(<MusicList releases={releases} />);
 
-    expect(getAllByTestId('release').length).toBe(3);
-    releases.forEach(({ node: release }) => {
+    expect(getAllByTestId('release').length).toBe(postAmount);
+    releases.forEach((release) => {
       const yearString = String(new Date(release.date).getFullYear());
       expect(getByText(release.title)).toBeInTheDocument();
       expect(getByText(yearString)).toBeInTheDocument();
       expect(getByText(release.description)).toBeInTheDocument();
-      expect(getByText(release.description)).toBeInTheDocument();
-      expect(getByText(release.description)).toBeInTheDocument();
+      expect(getAllByTestId('image').length).toBe(postAmount);
     });
   });
 });
