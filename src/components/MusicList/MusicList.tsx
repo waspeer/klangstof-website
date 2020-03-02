2e19c1415b60047f660438a8f8287185871da56b
Wannes Salomé <mail@wannessalome.nl>
2020-03-03 00:24:28 +0100

    Refactor to use shared components

--- src/components/MusicList/MusicList.tsx
+++ src/components/MusicList/MusicList.tsx
@@ -8,89 +8,115 @@ import {
   faSpotify,
   faYoutube,
 } from '@fortawesome/free-brands-svg-icons';
-import { faAlignLeft, faHeadphones } from '@fortawesome/free-solid-svg-icons';
+import { faAlignLeft, faHeadphones, faCaretLeft } from '@fortawesome/free-solid-svg-icons';
 import Image, { FluidObject } from 'gatsby-image';
 import React from 'react';
 import styled from 'styled-components';
 
+import Button from '#lib/components/Button';
 import ReleaseDTO from '#lib/types/Release';
-
-// ReleaseLink
-
-interface ReleaseLinkProps {
-  platform: string;
-  url: string;
-}
-
-const iconMap = new Map<string, [string, () => JSX.Element]>([
-  ['appleMusic', ['Apple Music', () => <FontAwesomeIcon icon={faItunes} />]],
-  ['deezer', ['Deezer', () => <FontAwesomeIcon icon={faAlignLeft} rotation={270} />]],
-  ['googlePlay', ['Google Play', () => <FontAwesomeIcon icon={faGooglePlay} />]],
-  ['itunes', ['iTunes Store', () => <FontAwesomeIcon icon={faApple} />]],
-  ['soundcloud', ['Soundcloud', () => <FontAwesomeIcon icon={faSoundcloud} />]],
-  ['spotify', ['Spotify', () => <FontAwesomeIcon icon={faSpotify} />]],
-  ['tidal', ['Tidal', () => <FontAwesomeIcon icon={faHeadphones} />]],
-  ['youtube', ['Youtube', () => <FontAwesomeIcon icon={faYoutube} />]],
-]);
-
-const ReleaseLink = ({ platform, url }: ReleaseLinkProps) => {
-  const [title, Icon] = iconMap.get(platform)!;
-
-  return (
-    <a data-testid={`releaseLink-${platform}`} href={url} title={title}>
-      <Icon /> {title}
-    </a>
-  );
-};
-
-// Release
+import { theme } from '#lib/theme';
+import { Title, Paragraph } from '#lib/components/Typography';
+import List from '#lib/components/List';
 
 type ReleaseProps = ReleaseDTO;
 
-const Front = styled.div``;
+const iconMap = new Map<string, [string, JSX.Element]>([
+  ['appleMusic', ['Apple Music', <FontAwesomeIcon icon={faItunes} />]],
+  ['deezer', ['Deezer', <FontAwesomeIcon icon={faAlignLeft} rotation={270} />]],
+  ['googlePlay', ['Google Play', <FontAwesomeIcon icon={faGooglePlay} />]],
+  ['itunes', ['iTunes Store', <FontAwesomeIcon icon={faApple} />]],
+  ['soundcloud', ['Soundcloud', <FontAwesomeIcon icon={faSoundcloud} />]],
+  ['spotify', ['Spotify', <FontAwesomeIcon icon={faSpotify} />]],
+  ['tidal', ['Tidal', <FontAwesomeIcon icon={faHeadphones} />]],
+  ['youtube', ['Youtube', <FontAwesomeIcon icon={faYoutube} />]],
+]);
 
-const Back = styled.div``;
+const ReleaseWrapper = styled.div`
+  height: 100%;
 
-const BackButton = styled.div``;
+  a {
+    color: #a8bdff;
+  }
+`;
 
-const Title = styled.h2``;
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-const Year = styled.small``;
+  & > *:first-child {
+    flex: 1;
+  }
+`;
 
-const Description = styled.p``;
+const Front = styled.div`
+  display: none;
+  backface-visibility: hidden;
+`;
 
-const Type = styled.i``;
+const Back = styled.div`
+  backface-visibility: hidden;
+  background-color: ${theme.colors.neutral};
+  color: ${theme.colors.background};
+  box-sizing: border-box;
+  padding: 2em;
+  overflow-y: scroll;
+  height: 100%;
+`;
 
-const Links = styled.ul``;
+const Type = styled.i`
+  opacity: 0.8;
+
+  &:before {
+    content: '(';
+  }
+
+  &:after {
+    content: ')';
+  }
+`;
 
 export const Release = ({ id, date, description, image, links, title, type }: ReleaseProps) => {
   const imageFluid = image.childImageSharp?.fluid as FluidObject;
   const year = new Date(date).getFullYear();
 
+  const linkList = links.length && (
+    <List
+      createKey={({ platform }) => `link-${id}-${platform}`}
+      items={links}
+      renderItem={({ platform, url }) => {
+        const [linkTitle, Icon] = iconMap.get(platform)!;
+        return (
+          <Button href={url} icon={Icon} testId={`releaseLink-${platform}`} type="link">
+            {linkTitle}
+          </Button>
+        );
+      }}
+      large
+    />
+  );
+
   return (
-    <div data-testid="release">
+    <ReleaseWrapper data-testid="release">
       <Front>
         <Image fluid={imageFluid} />
       </Front>
       <Back>
-        <BackButton />
-        <Title>{title}</Title>
-        <Year>{year}</Year>
-        <Description>
-          <Type>{type}</Type>
-          {description}
-        </Description>
-        {links.length && (
-          <Links>
-            {links.map(({ platform, url }) => (
-              <ul key={`link-${id}-${platform}`}>
-                <ReleaseLink platform={platform} url={url} />
-              </ul>
-            ))}
-          </Links>
-        )}
+        <Header>
+          <Title level={2} secondary={`${year}`}>
+            {title}
+          </Title>
+          <Button type="primary" icon={<FontAwesomeIcon icon={faCaretLeft} />}>
+            Back
+          </Button>
+        </Header>
+        <Paragraph>
+          <Type>{type}</Type> {description}
+        </Paragraph>
+        {linkList}
       </Back>
-    </div>
+    </ReleaseWrapper>
   );
 };
 
@@ -100,45 +126,52 @@ interface MusicListProps {
   releases: ReleaseDTO[];
 }
 
-const List = styled.ul`
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin-left: -20vw;
+`;
+
+const StyledList = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
-  pointer-events: auto;
+  padding-left: 20vw;
+  min-width: 100%;
+  height: calc(90vh - 20px);
+  display: flex;
+  overflow-x: auto;
 `;
 
 const ListItem = styled.li`
-  float: left;
-  width: calc(90vh - 20px);
-  height: calc(90vh - 20px);
-  margin: 5vh;
-  position: relative;
-  perspective: 1000px;
-
-  @media (orientation: 'portrait') {
-    width: calc(50vh - 20px);
-    height: calc(50vh - 20px);
-    margin: 25vh 5vh;
-  }
+  height: 100%;
+  overflow: hidden;
+  min-width: calc(90vh - 20px);
+  border: ${theme.colors.primary} 1px solid;
+  margin-right: 1rem;
 `;
 
 const MusicList = ({ releases }: MusicListProps) => {
   return (
-    <List>
-      {releases.map(({ id, date, description, image, links, title, type }) => (
-        <ListItem key={id}>
-          <Release
-            id={id}
-            date={date}
-            description={description}
-            image={image}
-            links={links}
-            title={title}
-            type={type}
-          />
-        </ListItem>
-      ))}
-    </List>
+    <Wrapper>
+      <StyledList>
+        {releases.map(({ id, date, description, image, links, title, type }) => (
+          <ListItem key={id}>
+            <Release
+              id={id}
+              date={date}
+              description={description}
+              image={image}
+              links={links}
+              title={title}
+              type={type}
+            />
+          </ListItem>
+        ))}
+      </StyledList>
+    </Wrapper>
   );
 };
 
